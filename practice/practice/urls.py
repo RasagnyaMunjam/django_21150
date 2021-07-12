ed294cfbf7c7a65a7750750f19db3a7a43a48c82
RasagnyaMunjam <81854131+RasagnyaMunjam@users.noreply.github.com>
2021-07-12 19:10:39 +0530

    practicee

---
+++ practice/practice/urls.py
@@ -0,0 +1,25 @@
+"""practice URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from prac import views as pr
+from prac2 import views as pr2
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("swng",pr.swing,name="swing"),
+    path("swng2",pr2.swng2,name="swing2")
+]
